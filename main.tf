fff08fca1222b37a2c7df63bf446ce3a03150e11
Manan <immnan333@gmail.com>
2023-01-16 15:04:25 +0530

    cap_v7

--- main.tf
+++ main.tf
@@ -23,14 +23,7 @@ provider "aws" {
 
 resource "aws_security_group" "k8saccess" {
   name   = "cap_access"
-  vpc_id = local.vpc_id
-    
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id = local.vpc_id 
 
   igress {
     from_port   = 0
